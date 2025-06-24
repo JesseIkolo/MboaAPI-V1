564b1e9db86fd8a45be76d21ed797de0b513d134
jesseikolo <jesseikoloj39@gmail.com>
2025-06-24 14:17:52 +0100

    Supprime l'import et l'appel à validateEnv qui n'est plus exporté dans config/env.js

--- mboawebapp/src/index.js
+++ mboawebapp/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { validateEnv } from './config/env';
-
-// Valider les variables d'environnement au démarrage
-validateEnv();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
